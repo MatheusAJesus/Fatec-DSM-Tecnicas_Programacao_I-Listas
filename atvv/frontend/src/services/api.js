4f7d8104f7bff2d884865fd58470deeefa358327
MatheusAJesus <matheus_augustoja@yahoo.com.br>
2022-12-01 21:18:46 -0300

    Novo frontend atvv

---
+++ atvv/frontend/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+
+axios.defaults.headers.common = {
+  "Content-Type": "application/json"
+}
+
+export const api = axios.create({
+  baseURL: "http://localhost:7071"
+})
